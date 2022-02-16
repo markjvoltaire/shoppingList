3180317ebed6353c35e5d7977d594c6791fbe337
Mark Voltaire <markjvoltaire@gmail.com>
2022-02-16 11:26:11 -0500

    passing test for adding item

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,24 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
-test('renders learn react link', () => {});
+test('if user can add an item', () => {
+  render(<App />);
+
+  //use user event to type into text box
+  // useUser event to click submit button
+  //expect item to appear on screen
+
+  const textbox = screen.getByRole('textbox');
+
+  const submitButton = screen.getByRole('button', {
+    name: /submit/i,
+  });
+
+  userEvent.type(textbox, 'apple');
+  userEvent.click(submitButton);
+
+  const item = screen.getByText(/apple/i);
+
+  expect(item).toBeInTheDocument();
+});
